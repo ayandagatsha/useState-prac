ad9d4a1f37211c6b6425ba38c105b405cc93a154
ayanda <64249187+ayandagatsha@users.noreply.github.com>
2021-03-08 20:41:24 +0400

    v1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 
 function App() {
+  let time = new Date().toLocaleTimeString();
+
+  time = time.split("PM");
+  let [timeValue, setTime] = React.useState(time);
+
+  function getTime() {
+    let time = new Date().toLocaleTimeString();
+    time = time.split("PM");
+    setTime((timeValue = time));
+  }
+  setInterval(getTime, 1000);
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1>{timeValue}</h1>
+      <button onClick={getTime}>Get Time</button>
     </div>
   );
 }
